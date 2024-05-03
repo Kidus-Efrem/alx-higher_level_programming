de0a8e5affb801e8295def2a65e380483d1ec511
kidus <kidusefrem2@gmail.com>
2024-05-03 20:45:33 +0300

    update task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined) {
-	console.log('No argument');
-  } else {
-	console.log(process.argv[2]);
-  }
-  
+const i = process.argv.length;
+if (i === 2) {
+  console.log('No argument');
+} else if (i === 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
